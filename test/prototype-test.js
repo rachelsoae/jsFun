ebdf1c79f290ee5aea44f748bd1af2f50ad0262e
Rachel Prather <rachelprather1@gmail.com>
2023-05-21 13:13:07 -0700

    Complete passing implementation code for weather tests

--- test/prototype-test.js
+++ test/prototype-test.js
@@ -376,29 +376,29 @@ describe("PROTOTYPES", () => {
   });
 
   describe("Weather prompts", () => {
-    it.skip("getAverageTemps", () => {
+    it("getAverageTemps", () => {
       const e = weatherPrompts.getAverageTemps();
 
       expect(e).to.deep.equal([
         40, 40, 44.5, 43.5, 57, 35, 65.5, 62, 14, 46.5
       ])
     }),
-      it.skip("findSunnySpots", () => {
-        const e = weatherPrompts.findSunnySpots();
+    it("findSunnySpots", () => {
+      const e = weatherPrompts.findSunnySpots();
 
-        expect(e).to.deep.equal(['Atlanta, Georgia is sunny.', 'New Orleans, Louisiana is sunny.', 'Raleigh, North Carolina is mostly sunny.']
-        )
-      }),
-      it.skip("findHighestHumidity", () => {
-        const e = weatherPrompts.findHighestHumidity();
+      expect(e).to.deep.equal(['Atlanta, Georgia is sunny.', 'New Orleans, Louisiana is sunny.', 'Raleigh, North Carolina is mostly sunny.']
+      )
+    }),
+    it("findHighestHumidity", () => {
+      const e = weatherPrompts.findHighestHumidity();
 
-        expect(e).to.deep.equal({
-          location: 'Portland, Oregon',
-          type: 'cloudy',
-          humidity: 84,
-          temperature: { high: 49, low: 38 }
-        })
+      expect(e).to.deep.equal({
+        location: 'Portland, Oregon',
+        type: 'cloudy',
+        humidity: 84,
+        temperature: { high: 49, low: 38 }
       })
+    })
   });
 
   describe("National Park Prompts", () => {
